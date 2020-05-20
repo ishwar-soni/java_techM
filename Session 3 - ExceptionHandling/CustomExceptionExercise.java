1b87e0ee6a83940821e68786d6db230df7f952f0
Sandeep Sinha <sandeep2official@gmail.com>
2020-05-21 02:19:58 +0530

    Session 3-  part5

---
+++ Session 3 - ExceptionHandling/CustomExceptionExercise.java
@@ -0,0 +1,16 @@
+package com.upGrad;
+
+public class CustomExceptionExercise {
+    //use CustomException here
+
+    public static void main(String[] args){
+        int result = Integer.MAX_VALUE, i;
+        System.out.println(1 & 1 );
+        System.out.println(2 & 1 );
+        System.out.println(23 & 1 );
+        System.out.println(172 & 1 );
+        System.out.println(result);
+    }
+}
+
+//create CustomException
