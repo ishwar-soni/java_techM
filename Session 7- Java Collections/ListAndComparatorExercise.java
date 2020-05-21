509a3b71b5ea4c31817fbe614c22db8dbb413534
Sandeep Sinha <sandeep2official@gmail.com>
2020-05-21 18:35:05 +0530

    Session 7- Part 1.b

---
+++ Session 7- Java Collections/ListAndComparatorExercise.java
@@ -0,0 +1,43 @@
+package com.upGrad;
+
+public class ListAndComparatorExercise {
+    public static void arrayListImplementation(){
+        //create ArrayList
+
+        //iterate using normal for loop
+
+        //iterate using forEach
+
+        //iterate using iterator
+    }
+
+    public static void linkedListImplementation(){
+        //create LinkedList
+
+        //iterate using normal for loop
+
+        //iterate using forEach
+
+        //iterate using iterator
+    }
+
+    public static void stackImplementation(){
+        //create Stack
+
+        //iterate using normal for loop
+
+        //iterate using forEach
+
+        //iterate using iterator
+    }
+
+    public static void vectorImplementation(){
+        //create Vector
+
+        //iterate using enumeration
+
+    }
+
+    
+
+}
