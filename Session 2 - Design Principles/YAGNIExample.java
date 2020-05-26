042180f4f064d1729a2f037ea0be8fba2bb55ed3
Sandeep Sinha <sandeep2official@gmail.com>
2020-05-26 15:19:32 +0530

    Session 2- part 7

---
+++ Session 2 - Design Principles/YAGNIExample.java
@@ -0,0 +1,22 @@
+package com.upGrad;
+
+public class YAGNIExample {
+    //functions for logging in
+    public boolean authenticateUser(String email, String password){
+        //logic
+        return true;
+    }
+    // DO NOT ADD THE FOLLOWING FUNCTIONS INITIALLY ,AS IT MIGHT NOT BE NEEDED LATER, AND WILL
+    //WASTE TIME AND RESOURCES NOW
+
+    /*//referrals will be added once 10000 active users are present
+    public String addUserReferrals(String userName){
+        //logic
+        return  "This user made referrals ";
+    }
+    //UserPreferences will be active once we have UserReferrals
+    public String createUserPreferences(String userName){
+        //logic
+        return "User has specific preferences";
+    }*/
+}
