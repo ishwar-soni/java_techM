230485d4c31a9e776fe26702785641986f7e3b94
Sandeep Sinha <sandeep2official@gmail.com>
2020-05-26 12:43:13 +0530

    Session 2- part 3

---
+++ Session 2 - Design Principles/LSPExercise.java
@@ -0,0 +1,16 @@
+package com.upGrad;
+
+class Bird{
+    public static void fly(String canBirdFly){
+        //code
+    }
+    public static void birdVoice(String birdCall){
+        //code
+    }
+}
+class ToyBird extends Bird{
+
+}
+
+public class LSPExercise {
+}
