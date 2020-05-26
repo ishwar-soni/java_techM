e974c254dd314f3eac2f33ff66ae0240c9962fb1
Sandeep Sinha <sandeep2official@gmail.com>
2020-05-26 11:14:59 +0530

    Session 2- part 1.b

---
+++ Session 2 - Design Principles/SingleResponsibilityPrincipleExercise.java
@@ -0,0 +1,21 @@
+package com.upGrad;
+
+
+class OnlineFoodOrder_Customer_Functions {
+    public static void orderInventory(String InventoryItemsList){
+        //logic goes here
+    }
+
+    public static void orderDish(String dishName){
+        //logic goes here
+    }
+
+    public static void totalBillAmount(int customerId){
+        //logic goes here
+    }
+
+    public static void cartItems(int customerId){
+        //logic goes here
+    }
+
+}
