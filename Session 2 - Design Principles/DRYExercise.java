3231d9785229477ce7f61e3d80c9ace2561065cb
Sandeep Sinha <sandeep2official@gmail.com>
2020-05-26 13:48:09 +0530

    Session 2- part 5

---
+++ Session 2 - Design Principles/DRYExercise.java
@@ -0,0 +1,38 @@
+package com.upGrad;
+
+public class DRYExercise {
+    public static int getStudentTotalMarks(String studentName){
+        int studentId = findStudentId(studentName);
+        int maths = findStudentMathsMarks(studentId);
+        int science = findStudentScienceMarks(studentId);
+        int total = maths + science;
+        return total;
+    }
+
+    public static String calculateGrade(String studentName){
+        int studentId = findStudentId(studentName);
+        int maths = findStudentMathsMarks(studentId);
+        int science = findStudentScienceMarks(studentId);
+        int total = maths + science;
+        String grade  = "";
+        if(total >= 50){
+            grade = "PASS";
+        }else{
+            grade = "FAIL";
+        }
+        return grade;
+    }
+    
+
+    public static int findStudentId(String studentName){
+        return 0;
+    }
+    public static int findStudentMathsMarks(int id){
+        return 0;
+    }
+    public static int findStudentScienceMarks(int id){
+        return 0;
+    }
+
+}
+
