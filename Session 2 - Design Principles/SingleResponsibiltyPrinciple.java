69c2175c2cfd323d4a29a9e86ccdf8792a276d10
Sandeep Sinha <sandeep2official@gmail.com>
2020-05-26 11:07:16 +0530

    Session 2- part 1

---
+++ Session 2 - Design Principles/SingleResponsibiltyPrinciple.java
@@ -0,0 +1,40 @@
+package com.upGrad;
+//class for admission desk
+public class SingleResponsibiltyPrinciple {
+    public static void addStudent(StudentLowCohesion student){
+        //code
+    }
+
+    public static int findStudentID(String studentName){
+        //code
+        return 0;
+    }
+
+    public static void studentAdmissionStatus(int studentId){
+        //codeLogic
+    }
+
+    public static void teacherSalaryStatus(int teacherId){
+        //codeLogic
+    }
+
+}
+
+class StudentFunctions{
+    public static void addStudent(StudentLowCohesion student){
+        //code
+    }
+    public static int findStudentID(String studentName){
+        //code
+        return 0;
+    }
+}
+
+class SchoolFunctions{
+    public static void teacherSalaryStatus(int teacherId){
+        //codeLogic
+    }
+    public static void studentAdmissionStatus(int studentId){
+        //codeLogic
+    }
+}
