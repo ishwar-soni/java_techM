0c846fc2364d7e8331ad74f2cb131f972534a6a0
Sandeep Sinha <sandeep2official@gmail.com>
2020-05-28 20:38:21 +0530

    Updated git logs

---
+++ Session 1 - Git/3a.HandsOn - git branch.sh
@@ -0,0 +1,58 @@
+# SandeepSinha@DESKTOP-OOM59NQ MINGW64 /d/gitExample (master)
+$ git branch newBranch
+
+# SandeepSinha@DESKTOP-OOM59NQ MINGW64 /d/gitExample (master)
+$ git checkout newBranch
+# Switched to branch 'newBranch'
+# M       File2.txt
+
+# SandeepSinha@DESKTOP-OOM59NQ MINGW64 /d/gitExample (newBranch)
+$ git status
+# On branch newBranch
+# Changes not staged for commit:
+#   (use "git add <file>..." to update what will be committed)
+#   (use "git restore <file>..." to discard changes in working directory)
+#         modified:   File2.txt
+
+# no changes added to commit (use "git add" and/or "git commit -a")
+
+# SandeepSinha@DESKTOP-OOM59NQ MINGW64 /d/gitExample (newBranch)
+$ git add .
+
+# SandeepSinha@DESKTOP-OOM59NQ MINGW64 /d/gitExample (newBranch)
+$ git status
+# On branch newBranch
+# Changes to be committed:
+#   (use "git restore --staged <file>..." to unstage)
+#         modified:   File2.txt
+
+
+# SandeepSinha@DESKTOP-OOM59NQ MINGW64 /d/gitExample (newBranch)
+$ git commit -m "New branch"
+# [newBranch 0af70a8] New branch
+#  1 file changed, 3 insertions(+), 1 deletion(-)
+
+
+# SandeepSinha@DESKTOP-OOM59NQ MINGW64 /d/gitExample (newBranch)
+$ git checkout master
+# Switched to branch 'master'
+# Your branch is up to date with 'origin/master'.
+
+# SandeepSinha@DESKTOP-OOM59NQ MINGW64 /d/gitExample (master)
+$ git merge newBranch
+# Updating 75e248b..0af70a8
+# Fast-forward
+#  File2.txt | 4 +++-
+#  1 file changed, 3 insertions(+), 1 deletion(-)
+
+# SandeepSinha@DESKTOP-OOM59NQ MINGW64 /d/gitExample (master)
+$ git push
+# Enumerating objects: 5, done.
+# Counting objects: 100% (5/5), done.
+# Delta compression using up to 4 threads
+# Compressing objects: 100% (3/3), done.
+# Writing objects: 100% (3/3), 318 bytes | 318.00 KiB/s, done.
+# Total 3 (delta 0), reused 0 (delta 0)
+# To https://github.com/sndpoffcl/gitDemo.git
+#    75e248b..0af70a8  master -> master
+
---
+++ Session 1 - Git/4.HandsOn - git clone, git pull.sh
@@ -0,0 +1,32 @@
+
+# SandeepSinha@DESKTOP-OOM59NQ MINGW64 ~/Desktop/gitClone
+$ git clone https://github.com/sndpoffcl/gitDemo.git
+# Cloning into 'gitDemo'...
+# remote: Enumerating objects: 4, done.
+# remote: Counting objects: 100% (4/4), done.
+# remote: Compressing objects: 100% (2/2), done.
+# remote: Total 4 (delta 0), reused 4 (delta 0), pack-reused 0
+# Unpacking objects: 100% (4/4), 254 bytes | 7.00 KiB/s, done.
+
+# SandeepSinha@DESKTOP-OOM59NQ MINGW64 ~/Desktop/gitClone
+$ cd gitDemo/
+
+# SandeepSinha@DESKTOP-OOM59NQ MINGW64 ~/Desktop/gitClone/gitDemo (master)
+$ git pull
+# remote: Enumerating objects: 5, done.
+# remote: Counting objects: 100% (5/5), done.
+# remote: Compressing objects: 100% (2/2), done.
+# remote: Total 3 (delta 0), reused 3 (delta 0), pack-reused 0
+# Unpacking objects: 100% (3/3), 273 bytes | 10.00 KiB/s, done.
+# From https://github.com/sndpoffcl/gitDemo
+#    2475b1d..75e248b  master     -> origin/master
+# Updating 2475b1d..75e248b
+# Fast-forward
+#  File2.txt | 1 +
+#  1 file changed, 1 insertion(+)
+
+# SandeepSinha@DESKTOP-OOM59NQ MINGW64 /d/gitExample (master)
+$ git fetch https://github.com/sndpoffcl/gitDemo.git
+# From https://github.com/sndpoffcl/gitDemo
+#  * branch            HEAD       -> FETCH_HEAD
+
