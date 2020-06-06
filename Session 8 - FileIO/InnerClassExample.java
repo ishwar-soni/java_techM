7f512c6d900de8383e568df54ff004a18967259c
Sandeep Sinha <sandeep2official@gmail.com>
2020-06-06 18:07:29 +0530

    inner classes updated

--- Session 8 - FileIO/InnerClassExample.java
+++ Session 8 - FileIO/InnerClassExample.java
@@ -1,74 +1,89 @@
 package com.upGrad;
 //Inner class example demonstrated
 
-class Outer {
-    // Simple nested inner class
-    class Inner {
-        public void show() {
-            System.out.println("In a nested class method");
+class Student{
+    //student has a inner class called Address
+    class Address{
+        String address = "Road, Locality , City";
+        public void displayAddress(){
+            System.out.println("Printing address inside the nested class");
         }
     }
 }
-class InnerClassExample {
-    public static void main(String[] args) {
-        Outer.Inner in = new Outer().new Inner();
-        in.show();
+public class InnerClassExample{
+    public static void main(string[] args){
+        Student.Address studentAddress = new Student().new Address();
+        studentAddress.displayAddress();
     }
 }
 
 //program to demonstrate Static Inner Class
-class OuterClassStatic {
-    private static void outerMethod() {
-        System.out.println("static outerMethod");
-    }
+class StudentOuter {
+    public static void displayAddress(){
+            System.out.println("Printing address from the Address class");
+        }
     // A static inner class
-    static class Inner {
+    static class Address {
+        String address = "Road, Locality , City";
         public static void main(String[] args) {
-            System.out.println("static inside inner class Method");
-            outerMethod();
+            System.out.println("Address will be printed here");
+            displayAddress();
         }
     }
 }
 
-//methodLocalInnerClass exampl
-
-class OuterClass {
-    void outerMethod() {
-        System.out.println("outerMethod");
-        // Inner class is local to outerMethod()
-        class Inner {
-            void innerMethod() {
-                System.out.println("innerMethod");
+//methodLocalInnerClass exampe
+class Student {
+    void createAddress() {
+        System.out.println("We are creating address object for student");
+        // Address class is local to createAddress()
+        class Address {
+            void displayAddress() {
+                System.out.println("Address is created for student");
             }
         }
-        Inner y = new Inner();
-        y.innerMethod();
+        Address address = new Address();
+        address.createAddress();
     }
 }
 class MethodLocalInnerClassExample {
     public static void main(String[] args) {
-        OuterClass x = new OuterClass();
-        x.outerMethod();
+        Student student = new Student();
+        student.createAddress();
     }
 }
 
-//program to demonstrate Anonymous inner class
-interface StudentsGrade{
-    String grade = "A";
-    void getStudentsGrade();
-}
-class AnonymousClassDemo{
+    //program to demonstrate Anonymous inner class
+    interface StudentsGrade{
+        String grade = "A";
+        void getStudentsGrade();
+    }
+    class AnonymousClassDemo{
+        public static void main(String[] args) {
+            //We have a  hidden inner class of StudentsGrade interface
+            // whose name is not written but an object to it
+            // is created.
+            StudentsGrade obj = new StudentsGrade() {
+                public void getStudentsGrade() {
+                    // printing grade
+                    System.out.print("Grade is "+ grade);
+                }
+            };
+            obj.getStudentsGrade();
+        }
+    }
+    class AnonymousClassDemo2{
+    static StudentsGrade obj = new StudentsGrade() {
+        public void getStudentsGrade() {
+            // printing grade
+            System.out.print("Grade is "+ grade);
+        }
+    };
+
     public static void main(String[] args) {
         //We have a  hidden inner class of StudentsGrade interface
         // whose name is not written but an object to it
         // is created.
-        StudentsGrade obj = new StudentsGrade() {
-            @Override
-            public void getStudentsGrade() {
-                // printing grade
-                System.out.print("Grade is "+ grade);
-            }
-        };
         obj.getStudentsGrade();
     }
 }
