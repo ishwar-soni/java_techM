502cdf04a02e19fdd4f7089fac089ed820f22cbd
ishwar-soni <59160606+ishwar-soni@users.noreply.github.com>
2020-06-08 12:57:10 +0530

    Update MethodSynchronizationExercise.java

--- Session 9 - MultiThreading/MethodSynchronizationExercise.java
+++ Session 9 - MultiThreading/MethodSynchronizationExercise.java
@@ -1,31 +1,37 @@
 package com.upGrad;
 class Account {
-    synchronized public void doAccountTransactions() {
-            System.out.println("Clerk will do transaction here ");
-            try {
-                Thread.sleep(400);
-            } catch (Exception e) {
-                System.out.println(e);
-            } 
-    } 
+	synchronized public void doAccountTransactions() {
+		System.out.println("Clerk started transaction.");
+		try {
+			Thread.sleep(400);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		System.out.println("Clerk Finished transaction.");
+	}
 }
-class Clerk extends Account {
-    Account account;
-    Clerk(Account account) {
-        this.account = account;
-    }
-    @Override
-    public void run() {
-        account.doAccountTransactions();
-    }}
+
+class Clerk extends Thread {
+	Account account;
+
+	Clerk(Account account) {
+		this.account = account;
+	}
+
+	@Override
+	public void run() {
+		account.doAccountTransactions();
+	}
+}
+
 public class MethodSynchronizationExercise {
-    public static void main(String[] args) {
-        Account account = new Account();
-        // we are creating two threads which share same Object.
-        Clerk clerk1 = new Clerk(account);
-        Clerk clerk2 = new Clerk(account);
-        // both threads start executing .
-        clerk1.start();
-        clerk2.start();
-    }
+	public static void main(String[] args) {
+		Account account = new Account();
+		// we are creating two threads which share same Object.
+		Clerk clerk1 = new Clerk(account);
+		Clerk clerk2 = new Clerk(account);
+		// both threads start executing .
+		clerk1.start();
+		clerk2.start();
+	}
 }
