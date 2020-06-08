82e70ee0931c110670ac20f7a8dfa4b377b053ab
Sandeep Sinha <sandeep2official@gmail.com>
2020-06-08 14:55:26 +0530

    updted block syn

--- Session 9 - MultiThreading/BlockSynchronization.java
+++ Session 9 - MultiThreading/BlockSynchronization.java
@@ -1,36 +1,36 @@
 package com.upGrad;
 
-import java.util.ArrayList;
-import java.util.List;
-
- class Count{
+class Count {
     int number = 0;
-    public void incrementNumber(){
-        synchronized (this) {
-            this.number++;
-        }
+
+    public void incrementNumber() {
+        this.number++;
     }
 }
- class Counter extends Thread{
+
+class Counter extends Thread {
     private Count count;
-    public Counter(Count counter){
+
+    public Counter(Count counter) {
         this.count = counter;
     }
+
     public void run() {
-        for(int i=0; i<5; i++){
-            count.incrementNumber();
-            System.out.println("Counter :" + count.number);
+        for (int i = 0; i < 5; i++) {
+            synchronized (this.count) {
+                count.incrementNumber();
+                System.out.println("Counter :" + count.number);
+            }
         }
     }
 }
 
-
-public class BlockSynchronizationExample{
-    public static void main(String[] args){
+public class BlockSynchronization {
+    public static void main(String[] args) {
         Count counter = new Count();
-        Counter  counter1 = new Counter(counter);
-        Counter  counter2 = new Counter(counter);
-        Counter  counter3 = new Counter(counter);
+        Counter counter1 = new Counter(counter);
+        Counter counter2 = new Counter(counter);
+        Counter counter3 = new Counter(counter);
         counter1.start();
         counter2.start();
         counter3.start();
