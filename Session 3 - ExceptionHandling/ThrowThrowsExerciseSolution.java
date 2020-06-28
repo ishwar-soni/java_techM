57eb3fafd8e262b3cbf0dfd5401d4ad2a3ed46cc
Sandeep Sinha <sandeep2official@gmail.com>
2020-06-28 12:08:52 +0530

    Update ThrowThrowsExerciseSolution.java

--- Session 3 - ExceptionHandling/ThrowThrowsExerciseSolution.java
+++ Session 3 - ExceptionHandling/ThrowThrowsExerciseSolution.java
@@ -4,11 +4,11 @@ public class BankServices {
     //in case we provie 0 months for emi calculation , how can we  use throw and throws 
     public static void calculateEMIAmount(int totalAmount, int months) throws Exception{
         int emiAmount = 0;
-            if(months <= 0){
-                throw new Exception("Number of months cant be less than 1");
-            }else{
-                emiAmount = totalAmount/months;
-                System.out.println("Emi amount : " + emiAmount);
-            }
+        if(months <= 0){
+            throw new Exception("Number of months cant be less than 1");
+        }else{
+            emiAmount = totalAmount/months;
+            System.out.println("Emi amount : " + emiAmount);
+        }
     }
 }
