07442e450821c2c785fe82cd4d5b4a6abeb90ec6
Sandeep Sinha <sandeep2official@gmail.com>
2020-06-28 12:12:07 +0530

    Update ThrowThrowsExerciseSolution.java

--- Session 3 - ExceptionHandling/ThrowThrowsExerciseSolution.java
+++ Session 3 - ExceptionHandling/ThrowThrowsExerciseSolution.java
@@ -1,6 +1,6 @@
 package com.upGrad;
 
-public class BankServices {
+public class EMICalculator {
     //in case we provie 0 months for emi calculation , how can we  use throw and throws 
     public static void calculateEMIAmount(int totalAmount, int months) throws Exception{
         int emiAmount = 0;
