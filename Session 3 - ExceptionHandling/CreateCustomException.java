19b4e8b83f14e1870f88d802e465675727b2b229
Sandeep Sinha <sandeep2official@gmail.com>
2020-06-29 13:13:56 +0530

    Update CreateCustomException.java

--- Session 3 - ExceptionHandling/CreateCustomException.java
+++ Session 3 - ExceptionHandling/CreateCustomException.java
@@ -1,40 +1,38 @@
 package com.upGrad;
 
-public class CreateCustomException {
-    public void finallyBlockExecution() {
-        String name = "upGrad";
-        try {
-            System.out.println("Name:" + name);
-            System.out.println("Exit from try block");
-            throw new MyCustomException("returns my custom message ");
-        } catch (MyCustomException ex) {
-            System.out.println("Catch block executed...");
-        } finally {
-            System.out.println("finally block executed");
+public class AccountDAOImpl {
+   //other methods and fields
+    public Account getAccountDetails(int accountNumber) throws Exception {
+        Account account = null;
+        for(Account acc : accounts){
+            if(acc.getAccountNumber() == accountNumber ){
+                return acc;
+            }
         }
-
-
+        System.out.println("Account for the given number does not exist");
+        throw new AccountDetailsNotFoundException("Account details not found for accoutNumber: "+ accountNumber);
     }
 }
 
     //creating CustomException
-    class MyCustomException extends Exception {
-        public MyCustomException() {
-        }
 
-        public MyCustomException(String message) {
-            super(message);
-        }
-
-        public MyCustomException(String message, Throwable cause) {
-            super(message, cause);
-        }
-
-        public MyCustomException(Throwable cause) {
-            super(cause);
-        }
-
-        public MyCustomException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-            super(message, cause, enableSuppression, writableStackTrace);
-        }
+public class AccountDetailsNotFoundException extends Exception {
+    public AccountDetailsNotFoundException() {
     }
+
+    public AccountDetailsNotFoundException(String message) {
+        super(message);
+    }
+
+    public AccountDetailsNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public AccountDetailsNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    public AccountDetailsNotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
